14e717d6a06e7769c6d2f415d02105c7ca9d02e0
Robert Blinde <robert@webbify.nl>
2019-11-26 16:47:50 +0100

    configured port number to work with Heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 /**
  * Randomly determines next move based on last one
